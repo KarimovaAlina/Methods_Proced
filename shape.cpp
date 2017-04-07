da279bd2c4d304e92411384f27758b5f9c7fbed4
KarimovaAlina <lina.ololo@mail.ru>
2017-04-07 09:23:05 +0700

    Lab2_class

--- shape.cpp
+++ shape.cpp
@@ -7,10 +7,15 @@ void In(parallelepiped &p, ifstream &ifst);
 void Out(sphere &s, ofstream &ofst);
 void Out(parallelepiped &p, ofstream &ofst);
 
+//New:
+void Out(tetrahedron &t, ofstream &ofst);
+void In(tetrahedron &t, ifstream &ifst);
+
 shape* In(ifstream &ifst) {
 	shape *sh;
 	sphere* sp;
 	parallelepiped* p;
+	tetrahedron* t;
 	int type;
 	ifst >> type;
 	switch (type)
@@ -31,6 +36,14 @@ shape* In(ifstream &ifst) {
 		sh = (shape*)p;
 		return sh;
 		break;
+	case 2:
+		t = new tetrahedron;
+		t->t = TETRAHEDRON;
+		In(*t, ifst);
+		ifst >> t->destiny;
+		sh = (shape*)t;
+		return sh;
+		break;
 	default:
 		return NULL;
 		break;
@@ -46,6 +59,10 @@ void Out(shape *s, ofstream &ofst) {
 	case PARALLELEPIPED:
 		Out(*((parallelepiped*)s), ofst);
 		ofst << ", Destiny = " << ((parallelepiped*)s)->destiny << endl;
+		break;
+	case TETRAHEDRON:
+		Out(*((tetrahedron*)s), ofst);
+		ofst << ", Destiny = " << ((tetrahedron*)s)->destiny << endl;
 		break;
 	default:
 		ofst << "Incorrect shape!" << endl;
